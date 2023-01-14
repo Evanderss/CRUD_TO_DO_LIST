97d4f5db3d9d3c0d22fd976aa7b974133ce7f8cb
Evanderss <evanderramirez150@gmail.com>
2023-01-13 22:54:30 -0600

    inicializando bootstrap5 con flask

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, make_response, redirect, render_template
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 todos = ["Finish this project", "Buy a coffe", "Sleep"]
